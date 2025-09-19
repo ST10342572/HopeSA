33ef1241ec35301315a79d68688b7ec563e36916
Christerbel Nwenya <130735393+ST10342572@users.noreply.github.com>
2025-09-19 14:25:25 +0200

    Add files via upload

---
+++ HopeSA_Application/settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        // Required for PayPal SDK
+        maven { url = uri("https://cardinalcommerceprod.jfrog.io/artifactory/android") }
+    }
+}
+
+rootProject.name = "HopeSA"
+include(":app")
